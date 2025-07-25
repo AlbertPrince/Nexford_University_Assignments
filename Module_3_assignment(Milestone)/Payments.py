38d920d46a12452ed02e429546c87b8754e5be45
AlbertPrince <apmensah@gmail.com>
2025-07-25 18:49:10 +0000

    added functions to policyholders and payments

--- Module_3_assignment(Milestone)/Payments.py
+++ Module_3_assignment(Milestone)/Payments.py
@@ -1,3 +1,6 @@
+import datetime
+
+
 class Payments:
     def __init__(self, amount, payment_date, policy_number):
         self.amount = amount
@@ -6,3 +9,13 @@ class Payments:
 
     def display_info(self):
         return f"Payment Amount: {self.amount}, Payment Date: {self.payment_date}, Policy Number: {self.policy_number}"
+    
+    def process_payment(self):
+        return f"Payment of {self.amount} for policy number {self.policy_number} has been processed on {self.payment_date}."
+    
+    def remind_payment(self):
+        payment_dt = datetime.datetime.strptime(self.payment_date, "%Y-%m-%d")
+        reminder_dt = payment_dt - datetime.timedelta(weeks=1)
+        today = datetime.datetime.today()
+        if today >= reminder_dt and today < payment_dt:
+            return f"Reminder: Payment of {self.amount} for policy number {self.policy_number} is due on {self.payment_date}."
--- Module_3_assignment(Milestone)/Policyholders.py
+++ Module_3_assignment(Milestone)/Policyholders.py
@@ -1,8 +1,22 @@
 class Policyholders:
-    def __init__(self, name, age, policy_number):
+    def __init__(self, name, age, policy_number, is_suspended=False, is_registered=False):
         self.name = name
         self.age = age
         self.policy_number = policy_number
+        self.is_suspended = is_suspended
+        self.is_registered = is_registered
 
     def display_info(self):
         return f"Policyholder Name: {self.name}, Age: {self.age}, Policy Number: {self.policy_number}"
+    
+    def register_policyholder(self):
+        self.is_registered = True
+        return f"Policyholder {self.name} has been registered."
+
+    def suspend_policyholder(self):
+        self.is_suspended = True
+        return f"Policyholder {self.name} has been suspended."
+    
+    def reactivate_policyholder(self):
+        self.is_suspended = False
+        return f"Policyholder {self.name} has been reactivated."
