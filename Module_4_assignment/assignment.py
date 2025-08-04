473e9d4f4700de90678413b499c9f9a0ffaa7ddf
AlbertPrince <apmensah@gmail.com>
2025-08-04 20:54:55 +0000

    Displayed top 10 most watched genres on Netflix

---
+++ Module_4_assignment/assignment.py
@@ -0,0 +1,46 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+
+file = pd.read_csv('Netflix_shows_movies.csv')
+
+print("number of missing values in each column:")
+print(file.isnull().sum())
+print("number of rows in the dataset: ", len(file))
+
+file['director'] = file['director'].fillna('Unknown')
+
+file['cast'] = file['cast'].fillna('Not entered')
+file['country'] = file['country'].fillna('Unknown')
+
+file_cleaned = file.dropna(subset=['rating', 'date_added'])
+
+file = file_cleaned
+print("number of rows after removing missing values: ", len(file))
+print("number of missing values in each column after cleaning:")
+print(file.isnull().sum())
+
+
+print(f'The dataset contains {file.shape[0]} rows and {file.shape[1]} columns.')
+print(f"Quick file overview: {file.head()}")
+print(f"Column names: {file.columns.tolist()}")
+print(f"Data types of each column: {file.dtypes}")
+print(f"Random sample of 5 rows:\n{file.sample(5)}")
+print(f"Summary of file: {file.info()}")
+print(f"Descriptive statistics:\n{file.describe()}")
+
+genres = file['listed_in'].str.split(', ').explode().str.strip()
+print(f"Unique genres: {genres.unique()}")
+
+#Finding the number of the top 10 most watched genres
+print(f"Number of unique genres: {genres.nunique()}")
+
+top_ten_genres = genres.value_counts().head(10)
+plt.figure(figsize=(10, 6))
+plt.barh(top_ten_genres.index, top_ten_genres.to_numpy(), color='skyblue')
+plt.xlabel('count')
+plt.ylabel('Genre')
+plt.title('Top 10 Most Watched Genres on Netflix')
+plt.tight_layout()
+plt.show()
+
+# file['date_added'] = pd.to_datetime(file['date_added'], errors='coerce')
